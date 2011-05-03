9615fb21762f86ae4cfc153ab67776fa2720c350
Jens Finkhaeuser <jens@finkhaeuser.de>
2011-05-03 17:44:33 +0100

    Integrate sass stylesheets

--- service/config/application.rb
+++ service/config/application.rb
@@ -18,7 +18,7 @@ module Service
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-    config.plugins = [ :dynamic_form ]
+    config.plugins = [ :dynamic_form, :sass ]
 
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
