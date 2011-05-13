09f2ef6e272266f5deb485f8598550dcb54fe0d0
Jens Finkhaeuser <jens@finkhaeuser.de>
2011-05-13 21:04:58 +0100

    Quote page names, makes it easier to understand they're symbolic.

--- service/features/support/paths.rb
+++ service/features/support/paths.rb
@@ -19,7 +19,7 @@ module NavigationHelpers
 
     else
       begin
-        page_name =~ /the (.*) page/
+        page_name =~ /the "(.*)" page/
         path_components = $1.split(/\s+/)
         self.send(path_components.push('path').join('_').to_sym)
       rescue Object => e
