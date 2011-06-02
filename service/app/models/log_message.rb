c59f58d0ad750a640aee9117e4bd9dd494663301
Jens Finkhaeuser <jens@finkhaeuser.de>
2011-06-02 22:54:41 +0100

    Should've been part of previous commit

--- service/app/models/log_message.rb
+++ service/app/models/log_message.rb
@@ -1,4 +1,4 @@
 class LogMessage < ActiveRecord::Base
-  belongs_to :stacktrace
+  has_and_belongs_to_many :occurrences
   validates_uniqueness_of :tag, :scope => [:message]
 end
--- service/app/models/occurrence.rb
+++ service/app/models/occurrence.rb
@@ -1,4 +1,5 @@
 class Occurrence < ActiveRecord::Base
   belongs_to :stacktrace
+  has_and_belongs_to_many :log_messages
   validates_uniqueness_of :phone, :scope => [:stacktrace_id, :os_version]
 end
