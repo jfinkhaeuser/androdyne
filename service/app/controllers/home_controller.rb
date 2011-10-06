9af7354fb4db7acebb1d4ab9bf23c7949872f3c7
Jens Finkhaeuser <jens@finkhaeuser.de>
2011-10-06 09:43:00 +0100

    Something in will_paginate changed, and it's no longer possible to paginate arrays. Then again, that wasn't the best of ideas in the first place.

--- service/app/controllers/home_controller.rb
+++ service/app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
-#  before_filter :require_user # FIXME
 
   def index
     if current_user
-      @packages = current_user.packages.all.paginate(:page => params[:page], :per_page => 5, :order => 'name ASC')
+      @packages = current_user.packages.paginate(:page => params[:page], :per_page => 5, :order => 'name ASC')
     end
   end
 
